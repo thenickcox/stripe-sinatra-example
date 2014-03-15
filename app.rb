ffe5445f571799eb45b03fe2a811a2d4c410a44c
Nick Cox <nick@nvigatingcancer.com>
2014-03-15 15:45:49 -0700

    Passing capybara test for filling out the form

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'stripe'
+require 'haml'
 
+<<<<<<< HEAD
 Stripe.api_key = "STRIPE_API_PRIVATE_KEY"
 set :views, File.dirname(__FILE__) + "/views"
 
@@ -24,5 +26,35 @@ post "/pay" do
     erb :thanks
   else
     redirect "/"
+=======
+Stripe.api_key = "sk_test_AzK176W5YbWlpbJIQR8VXRNU"
+set :views, File.dirname(__FILE__) + "/views"
+
+class App < Sinatra::Base
+
+  get "/" do
+    haml :form
+  end
+
+  post "/pay" do
+    amount = params[:amount].to_i
+    Stripe::Customer.create(
+      email: params[:email],
+      metadata: {
+        name: params[:name],
+        address: params[:address],
+        city: params[:city],
+        state: params[:state],
+        zip: params[:zip]
+      }
+    )
+    @charge = Stripe::Charge.create(
+      amount: amount,
+      currency: "usd",
+      card: params[:stripeToken],
+      description: "test payment"
+    )
+    haml :thanks
+>>>>>>> 09c2c03... Passing capybara test for filling out the form
   end
 end
