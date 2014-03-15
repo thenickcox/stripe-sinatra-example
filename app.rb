3d1a76b7a4b232a6b424ee4cf13c91a05409499a
Nick Cox <nick@nvigatingcancer.com>
2014-03-15 15:58:03 -0700

    Clean up after deleting previous commit

--- app.rb
+++ app.rb
@@ -3,30 +3,6 @@ require 'sinatra'
 require 'stripe'
 require 'haml'
 
-<<<<<<< HEAD
-Stripe.api_key = "STRIPE_API_PRIVATE_KEY"
-set :views, File.dirname(__FILE__) + "/views"
-
-def is_number?(i)
-  true if Float(i) rescue false
-end
-
-get "/" do
-  erb :form
-end
-
-post "/pay" do
-  if is_number?(params[:amount].to_f)
-    amount = ((params[:amount].to_f)*100).to_i
-    @charge = Stripe::Charge.create(
-      :amount => amount,
-      :currency => "usd",
-      :card => params[:stripeToken],
-      :description => "test payment")
-    erb :thanks
-  else
-    redirect "/"
-=======
 Stripe.api_key = "sk_test_AzK176W5YbWlpbJIQR8VXRNU"
 set :views, File.dirname(__FILE__) + "/views"
 
@@ -55,6 +31,5 @@ class App < Sinatra::Base
       description: "test payment"
     )
     haml :thanks
->>>>>>> 09c2c03... Passing capybara test for filling out the form
   end
 end
